aa3e43d600d5835bcb8329c293deffbedc9cbdb0
ak-umka <umbet70@gmail.com>
2022-06-13 19:24:45 +0600

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import GlobalFeed from '@/views/GlobalFeed'
 import YourFeed from '@/views/YourFeed'
+import TagFeed from '@/views/TagFeed'
 import Register from '@/views/Register'
 import Login from '@/views/Login'
 
@@ -28,7 +29,7 @@ const routes = [
   {
     path: '/tags/:slug',
     name: 'tag',
-    component: GlobalFeed
+    component: TagFeed
   },
   {
     path: '/articles/new',
